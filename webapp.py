8c3688103da5f194642ff8d3ccabd45a464f22e9
Kelechi Opara <kaycopara911@gmail.com>
2025-07-14 04:07:51 +0100

    added new features(message flashing)

--- webapp.py
+++ webapp.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=5)
 
 @app.route("/")
 def home():
@@ -10,13 +12,14 @@ def home():
 @app.route("/login", methods= ["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        flash("Login Succesful!")
         return redirect(url_for("user"))
     else:  
         if "user" in session:
-            return redirect(url_for('user'))
-        
+            return redirect(url_for('user')) 
         return render_template("login.html")
   
 
@@ -24,13 +27,15 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>" 
+        return render_template('user.html', user=user)
     else:
+        flash("You are not logged in!")
         return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
+    flash("You have been logged out", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
 
